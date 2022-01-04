682cf07fb84a12d0bbb790614525a5c39e7c3f96
Mark Corpuz <social@markcorpuz.com>
2022-01-04 09:58:13 -0800

    1.1 | Updated cta images and donation link
    - redirected url for images to point towards the themes cta images
    - changed donation link towards an updated version

--- urc-cta.php
+++ urc-cta.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: URC CTA
  * Description: Show call to action buttons for accessibility.
- * Version: 1.0
+ * Version: 1.1
  * Author: Jake Almeda
  * Author URI: http://smarterwebpackages.com/
  * Network: true
@@ -40,7 +40,7 @@ function urc_ctas() {
         
     $upload_dir = wp_upload_dir();
     
-    $donate_page = 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LKGTSSLYJ93J6';
+    $donate_page = 'https://www.paypal.com/donate/?hosted_button_id=KNH8SDFGVT8UC';
     
     //$phone_coaching_page = get_permalink( get_page_by_path( "phone-coaching" ) );
     $phone_coaching_page = get_permalink( '22818' );
@@ -53,25 +53,25 @@ function urc_ctas() {
         
         if( $post && !in_array( $post->post_name, urc_cta_hidden() ) ) :
             echo '<div class="module cta-icon"><div class="module-wrap">
-                    <div><a class="item image link" href="'.$free_ebook_page.'" data-type="page"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-free-ebook-icon.png" alt="" class="wp-image-41165" width="50" height="50"></a></div>
+                    <div><a class="item image link" href="'.$free_ebook_page.'" data-type="page"><img src="https://understandingrelationships.com/wp-content/themes/cor-2021/assets/images/cta/cta-mobile-free-ebook-icon.png" alt="" width="50" height="50" loading="lazy"/></a></div>
                     <div class="items info"><div><a class="item title link" href="'.$free_ebook_page.'" data-type="page" data-id="1519">Free eBooks & Audio Program</a></div></div>
                 </div></div>';
         endif;
 
         echo '<div class="module cta-icon"><div class="module-wrap">
-                <div><a class="item image link" href="'.$phone_coaching_page.'" data-type="page"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-coaching-icon.png" alt="" class="wp-image-41163" width="50" height="50"></a></div>
+                <div><a class="item image link" href="'.$phone_coaching_page.'" data-type="page"><img src="https://understandingrelationships.com/wp-content/themes/cor-2021/assets/images/cta/cta-mobile-coaching-icon.png" alt="" width="50" height="50" loading="lazy"/></a></div>
                 <div class="items info"><div><a class="item title link" href="'.$phone_coaching_page.'" data-type="page" data-id="1519">Phone/Skype Coaching Session</a></div></div>
             </div></div>
             <div class="module cta-icon"><div class="module-wrap">
-                <div><a class="item image link" href="https://teespring.com/stores/coach-corey-wayne"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-products-icon.png" alt="" class="wp-image-41161" width="50" height="50"></a></div>
+                <div><a class="item image link" href="https://teespring.com/stores/coach-corey-wayne"><img src="https://understandingrelationships.com/wp-content/themes/cor-2021/assets/images/cta/cta-mobile-products-icon.png" alt="" width="50" height="50" loading="lazy"/></a></div>
                 <div class="items info"><div><a class="item title link" href="https://teespring.com/stores/coach-corey-wayne">Coach Corey Wayne Merchandise</a></div></div>
             </div></div>
             <div class="module cta-icon"><div class="module-wrap">
-                <div><a class="item image link" href="'.$donate_page.'" data-type="page" ><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-donate-icon.png" alt="" class="width="50" height="50"></a></div>
+                <div><a class="item image link" href="'.$donate_page.'" data-type="page" ><img src="https://understandingrelationships.com/wp-content/themes/cor-2021/assets/images/cta/cta-mobile-donate-icon.png" alt="" width="50" height="50" loading="lazy"/></a></div>
                 <div class="items info"><div><a class="item title link" href="'.$donate_page.'">Make A Donation</a></div></div>
             </div></div>
             <div class="module cta-icon"><div class="module-wrap">
-                <div><a class="item image link" href="'.$products_page.'" data-type="page"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-all-books-icon.png" alt="" class="wp-image-41162" width="50" height="50"></a></div>
+                <div><a class="item image link" href="'.$products_page.'" data-type="page"><img src="https://understandingrelationships.com/wp-content/themes/cor-2021/assets/images/cta/cta-mobile-bookset-all-books-icon.png" alt="" width="50" height="50" loading="lazy"/></a></div>
                 <div class="items info"><div><a class="item title link" href="'.$products_page.'" data-type="page">Books & Recommended Products</a></div></div>
             </div></div>';
             
